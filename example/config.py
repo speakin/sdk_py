fd9e4e7288bab949f942f938b565c576c92a729e
lixianliang <lixianliang@speakin.mobi>
2018-08-31 15:30:09 +0800

    up2

--- example/config.py
+++ example/config.py
@@ -1,13 +1,13 @@
 config = {
     "app": {
         "name": "app-test",
-        "accessKey": "e452db5c49091461a7e10760bdfc4a9b",
-        "secretKey": "edb4e576bddc27d7c530ff44ec8de2c4",
+        "accessKey": "xxxxxxxxx",
+        "secretKey": "xxxxxxxxx",
     },
     "bucket": {
         "name": "bucket-test",
-        "accessKey": "fe20e17ebddbfd546edeccf0e390f29e",
-        "secretKey": "4d79940c599330c0888676b1ea402822",
+        "accessKey": "xxxxxxxxx",
+        "secretKey": "xxxxxxxx",
     },
     "unionID": "py_user1",
     "registerFiles": ["../testdata/text/u1/01_16k.wav", "../testdata/text/u1/02_16k.wav", "../testdata/text/u1/03_16k.wav"],
@@ -18,13 +18,13 @@ config = {
 config_digital = {
     "app": {
         "name": "app-test",
-        "accessKey": "e452db5c49091461a7e10760bdfc4a9b",
-        "secretKey": "edb4e576bddc27d7c530ff44ec8de2c4",
+        "accessKey": "xxxxxxxxx",
+        "secretKey": "xxxxxxxx",
     },
     "bucket": {
         "name": "bucket-test",
-        "accessKey": "fe20e17ebddbfd546edeccf0e390f29e",
-        "secretKey": "4d79940c599330c0888676b1ea402822",
+        "accessKey": "xxxxxxxxx",
+        "secretKey": "xxxxxxxx",
     },
     "unionID": "py_user2",
     "registerFiles": ["../testdata/digital/u1/01_16k.wav", "../testdata/digital/u1/02_16k.wav", "../testdata/digital/u1/03_16k.wav"],
